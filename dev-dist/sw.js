67afc010bdb6b75075d5681d26c09dfd5f90e924
AlexKim333 <tukpan71@gmail.com>
2026-07-24 11:27:19 -0600

    fix(ai): add '한돌' to wake word and implement voice cancellation logic

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f1gr2rk6uoo"
+    "revision": "0.fbas46b0un8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
