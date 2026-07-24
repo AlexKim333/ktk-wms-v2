efed1202e477d99dd98d24ad028cd5a61426f16c
AlexKim333 <tukpan71@gmail.com>
2026-07-24 12:00:02 -0600

    chore(ai): remove general chat feature to save API tokens

--- src/views/PosView.vue
+++ src/views/PosView.vue
@@ -2747,10 +2747,6 @@ const handleSamdoriIntent = (intentObj) => {
     } else {
       submitReservation()
     }
-  } else if (intent === 'chat') {
-    if (samdori.value && intentObj.message) {
-      samdori.value.speak(intentObj.message)
-    }
   }
 }
 </script>
